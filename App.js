e2d1800267c7a95a993f95b71f123da3105caee5
mejrisalma <mejri123salma@gmail.com>
2021-01-09 19:49:04 +0100

    node

---
+++ App.js
@@ -0,0 +1,25 @@
+// const Math= require("./Math");
+// console.log(Math);
+
+//  console.log("welcome i am node js");
+//  const sum = (a,b)=>a+b;
+//  console.log("the sum is ",sum(8,2))
+//   console.log(process);
+//  a = process.argv[2];
+//  b =process.argv[3];
+
+// switch(operations){
+//     case"+":
+//     console.log('the sum of ${a} is ${Math.sum(a,b)})');
+//     break;
+//     case"-":
+//     console.log('the minus of ${a} is ${Math.minus(a,b)})');
+//     break;
+//     case"*":
+//     console.log('the multiply of ${a} is ${Math.multiply(a,b)})');
+//     break;
+//     default:
+//         console.log("something is not oky ???? be carful" );
+// }
+const fs =require ("fs");
+console.log(fs);
---
+++ code.txt
@@ -0,0 +1,9 @@
+const sum = (a,b)=> a+b;
+const minus = (a,b)=> a+b;
+const multiply= (a,b)=> a+b;
+module.exports ={
+    sum ,
+    minus,
+    multiply,
+    
+}
